d6075dc7f6484f5460561fa148050b5fccc06a6a
ida64pro <skew720@gmail.com>
2024-05-03 21:40:00 +0300

    Source a program
    Source sending packets to an IP Address

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.NetworkInformation;
+
+namespace Limiter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Skew SoftWare";
+            string host = Dns.GetHostName();
+            Console.Write("Приветствую " + host + ", это Skew Soft. Введите Ваш IP Adress: ");
+            var ipAddress = Console.ReadLine();
+            int timeout = 450; // Таймаут в миллисекундах
+
+            Ping pingSender = new Ping();
+
+            for (; ; )
+            {
+                PingReply reply = pingSender.Send(ipAddress, timeout);
+
+                if (reply.Status == IPStatus.Success)
+                {
+                    Console.WriteLine("Пакет отправлен.");
+                }
+                else
+                {
+                    Console.WriteLine("Пакет не отправлен. Статус: " + reply.Status);
+                }
+            }
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,38 @@
+﻿using System.Resources;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Общие сведения об этой сборке предоставляются следующим набором
+// набора атрибутов. Измените значения этих атрибутов для изменения сведений,
+// связанные с этой сборкой.
+[assembly: AssemblyTitle("Skew Software")]
+[assembly: AssemblyDescription("Program by skew to sending packets. Optimized for VimeWorld.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Skew Software")]
+[assembly: AssemblyCopyright("Copyright © 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка значения False для параметра ComVisible делает типы в этой сборке невидимыми
+// для компонентов COM. Если необходимо обратиться к типу в этой сборке через
+// из модели COM задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID представляет идентификатор typelib, если этот проект доступен из модели COM
+[assembly: Guid("707ed4c3-c68c-4ce1-8639-22c742937158")]
+
+// Сведения о версии сборки состоят из указанных ниже четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии
+//      Номер сборки
+//      Номер редакции
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("2.0.0.0")]
+[assembly: NeutralResourcesLanguage("ru")]
